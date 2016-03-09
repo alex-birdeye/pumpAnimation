67da6009589fba6acd0f58ce0bb95d1d70d9d285
youser <youser@mail.mail>
2016-03-09 23:36:38 +0200

    Main indicator added.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ function log(msg) {
     console.log(msg);
 }
 
+var delta = 0;
 $(document).ready(function () {
     doPoll();
 });
@@ -17,6 +18,46 @@ function doPoll() {
             motorRun(motorRunStatus);
             onLoad(onLoadStatus);
             lastState(motorRunStatus, onLoadStatus);
+
+            jQuery('[id="indicatorContainer"]').jqxGauge({
+                ranges: [{
+                    startValue: 0,
+                    endValue: 55,
+                    style: {fill: '#C9C9C9', stroke: '#C9C9C9'},
+                    endWidth: 5,
+                    startWidth: 1
+                },
+                    {
+                        startValue: 55,
+                        endValue: 110,
+                        style: {fill: '#FCF06A', stroke: '#FCF06A'},
+                        endWidth: 10,
+                        startWidth: 5
+                    },
+                    {
+                        startValue: 110,
+                        endValue: 165,
+                        style: {fill: '#FCA76A', stroke: '#FCA76A'},
+                        endWidth: 15,
+                        startWidth: 10
+                    },
+                    {
+                        startValue: 165,
+                        endValue: 220,
+                        style: {fill: '#FC6A6A', stroke: '#FC6A6A'},
+                        endWidth: 20,
+                        startWidth: 15
+                    }],
+                ticksMinor: {interval: 5, size: '5%'},
+                ticksMajor: {interval: 10, size: '9%'},
+                value: 0,
+                colorScheme: 'scheme03',
+                animationDuration: 1000
+            });
+            var oilPressure = data.compressors[0].oil_pressure;
+            var value = parseInt(oilPressure, 10) + delta;
+            jQuery('[id="indicatorContainer"]').jqxGauge('value', value);
+            delta += 5;
             setTimeout(doPoll, 1000);
         },
         error: function (XMLHttpRequest, textStatus, error) {
@@ -26,7 +67,7 @@ function doPoll() {
 }
 
 function lastState(motorRunStatus, onLoadStatus) {
-    if(!motorRunStatus && !onLoadStatus){
+    if (!motorRunStatus && !onLoadStatus) {
         startVioletPipe1();
         Preview_violetarrowssmall();
         Preview_violetarrowssmall2_();
