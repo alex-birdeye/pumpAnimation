7dfbf2cb944429033c5b9cb22021c93d47a8954a
youser <you@example.com>
2016-07-23 20:29:35 +0000

    Real time calendar added.

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ var blocksCount = compressorBlocksArray.length;
 
 $(document).ready(function () {
     doPoll();
+    date_time("date-time");
 });
 
 function doPoll() {
@@ -143,6 +144,37 @@ function doPoll() {
             alert("error\n" + XMLHttpRequest + "\n" + textStatus + "\n" + error);
         }
     });
+}
+
+function date_time(id)
+{
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    h = date.getHours();
+    if(h<10)
+    {
+        h = "0"+h;
+    }
+    m = date.getMinutes();
+    if(m<10)
+    {
+        m = "0"+m;
+    }
+    s = date.getSeconds();
+    if(s<10)
+    {
+        s = "0"+s;
+    }
+    // result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+    result = ''+d+'.'+month+'.'+year+' '+h+':'+m+':'+s;
+    document.getElementById(id).innerHTML = result;
+    setTimeout('date_time("'+id+'");','1000');
+    return true;
 }
 
 function lastState(motorRunStatus, onLoadStatus) {
